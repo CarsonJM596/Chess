f21a75f28aba55fa5791bdc2db8a00248f0740e0
CarsonJM596 <124809808+CarsonJM596@users.noreply.github.com>
2025-01-25 19:40:24 -0600

    Legal moves

--- chess.js
+++ chess.js
@@ -73,6 +73,9 @@ function getLegalMoves(startingSqId, piece){ //Gets all legal moves for all piec
     if (piece.classList.contains("knight")){
         getKnightMoves(startingSqId, pieceColor);
     }
+    if (piece.classList.contains("rook")){
+        getRookMoves(startingSqId, pieceColor);
+    }
 }
 function isSquareOcc(square){//Returns what piece color square is occupied by 
     if (square.querySelector(".piece")){
@@ -132,14 +135,15 @@ function checkPawnForwardMoves(startingSqId, pieceColor) { //Checks for all lega
         }
     }
 }
-function getKnightMoves(startingSqId, pieceColor){
-    const file = startingSqId.charCodeAt(0)-97;
-    const rank = startingSqId.charAt(1);
+function getKnightMoves(startingSqId, pieceColor) {
+    const file = startingSqId.charCodeAt(0) - 97;
+    const rank = parseInt(startingSqId.charAt(1), 10);
     const rankNum = parseInt(rank);
-    let currRank = rankNum;
+    let currFile = file;
+    let currRank = rank;
 
     const moves = [
-        [-2, 1], 
+        [-2, 1],
         [-1, 2],
         [1, 2],
         [2, 1],
@@ -147,14 +151,20 @@ function getKnightMoves(startingSqId, pieceColor){
         [1, -2],
         [-1, -2],
         [-2, -1],
-    ]; //All Possible knight moves
+    ];
 
-    moves.forEach((move) =>{
-        currFile = file+move[0];
-        currRank = rankNum + move[1];
-        if (currFile > 0 && currFile < 7 && currRank>0  && currRank < 8){
-            
+    moves.forEach((move) => {
+        currFile = file + move[0];
+        currRank = rankNum + move[1]
+        if((currFile >= 0 && currFile <= 7) &&(currRank >= 0 && currRank <= 8)){
+            let currentSqId = String.fromCharCode(currFile+97)+currRank;
+            let currentSq = document.getElementById(currentSqId);
+            let sqContent = isSquareOcc(currentSq);
+            if (sqContent != "blank" && sqContent == pieceColor){
+                return;
+            }
+            legalSquares.push(String.fromCharCode(currFile+97)+currRank);
         }
-    })
-
+    });
+    
 }
