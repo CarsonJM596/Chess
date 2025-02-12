e88e02cdab23e8a4af9f44965a7c7d7ae3de120c
CarsonJM596 <124809808+CarsonJM596@users.noreply.github.com>
2025-02-12 16:17:07 -0600

    promotion + new style

--- js/queen.js
+++
@@ -1,6 +0,0 @@
-function getQueenMoves(startingSquareId, pieceColor, boardSquaresArray) {
-    let bishopMoves = getBishopMoves(startingSquareId,pieceColor,boardSquaresArray);
-    let rookMoves = getRookMoves(startingSquareId, pieceColor, boardSquaresArray);
-    let legalSquares = [...bishopMoves, ...rookMoves];
-    return legalSquares;
-}
